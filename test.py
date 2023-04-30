33ef97c5a34c26f72dc1a5e982dd3f39ed98f62c
Lily Zihui Zhu <zhuzihui.lily@gmail.com>
2023-04-30 12:12:16 -0400

    implemented top k accuracy calculation

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ Implements the testing procedure and various cloze task metrics
 
 import torch
 from transformers import BertForMaskedLM, BertTokenizer
-from utils import load_test_data
+from utils import load_test_data, load_test_target_words
  
 
 def get_topk_predictions(model_path='bert-base-uncased', k=5):
@@ -35,23 +35,43 @@ def get_topk_predictions(model_path='bert-base-uncased', k=5):
     line_id, top_k_preds, line_preds = 0, [], [None] * k
 
     for i in range(len(test_line_ids)): # For each masked token 
-        if test_line_ids[i] > line_id:  # If the line id changes, save the tokens for the previous line
-            line_id += 1                # and reinitialize the placeholders
-            # Convert the tokens into a string (final prediction for the original word)
-            top_k_preds.append([tokenizer.convert_tokens_to_string(tokens) for tokens in line_preds])
-            line_preds = [None] * k
         token_id = masked_token_ids[i] # Get masked token id within that line (can have multiple)
         # Convert the top k token ids into tokens
         preds = tokenizer.convert_ids_to_tokens(torch.topk(masked_outputs_logits[line_id,token_id], k).indices)
         # Each target word might have gotten broken down into several tokens 
         # Put the token predictions for the same word together in a list 
         line_preds = [old + [new] if old else [new] for old, new in zip(line_preds, preds)]
-    
+
+        if i == len(test_line_ids) - 1 or test_line_ids[i+1] > line_id:  # If the line id changes, save the tokens for the previous line
+            line_id += 1                # and reinitialize the placeholders
+            # Convert the tokens into a string (final prediction for the original word)
+            top_k_preds.append([tokenizer.convert_tokens_to_string(tokens) for tokens in line_preds])
+            line_preds = [None] * k
+
     return top_k_preds
 
-def get_topk_accuracy(k=5):
-    # TODO
-    raise NotImplementedError
+def get_topk_accuracy(top_k_preds, targets):
+    """
+    Computes top k accuracy of the model predictions
+    If any of the top k predictions matches the target word, score it as correct
+
+    Args:
+        top_k_preds (list): output of `get_topk_predictions()`
+        targets (list): list of target words (ground truths), output of `utils.load_test_target_words()`
+
+    Returns:
+        accuracy (float): number of correct top k predictions / total number of targets to predict
+    """
+    assert len(top_k_preds) == len(targets) # Sanity check
+    accuracy_ct = 0
+    for i in range(len(targets)):
+        for j in range(len(top_k_preds[i])):
+            if top_k_preds[i][j] == targets[i]:
+                accuracy_ct += 1
+                print(top_k_preds[i], targets[i])
+                break
+    
+    return accuracy_ct / len(targets)
 
 def get_topk_cossim(k=5):
     # TODO
@@ -61,6 +81,6 @@ def get_topk_rhyme(k=5):
     # TODO
     raise NotImplementedError
 
-        
+
         
 
