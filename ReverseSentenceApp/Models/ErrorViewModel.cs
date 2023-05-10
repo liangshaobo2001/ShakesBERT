ee42c2c9e61ff41b641e6d75db0c6ee7ef4b102c
FrozenPeak0701 <73618481+FrozenPeak0701@users.noreply.github.com>
2023-05-10 02:28:40 -0400

    update

---
+++ ReverseSentenceApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ReverseSentenceApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ ReverseSentenceApp/bin/Debug/net7.0/reverse_sentence.py
@@ -0,0 +1,10 @@
+import sys
+
+def reverse_sentence(sentence: str) -> str:
+    words = sentence.split()
+    reversed_words = ' '.join(reversed(words))
+    return reversed_words
+
+if __name__ == "__main__":
+    input_sentence = sys.argv[1]
+    print(reverse_sentence(input_sentence))
